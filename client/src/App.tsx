38352c8639af266f7e03543122b20946dd709773
parveen barak <parveenbarak117@gmail.com>
2024-10-27 11:51:39 +0530

    Auth and Mainlyout  Frontend created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from './components/ui/button'
+import Login from "./auth/Login";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Signup from "./auth/Signup";
+import ForgotPassword from "./auth/ForgotPassword";
+import ResetPassword from "./auth/ResetPassword";
+import VerifyEmail from "./auth/VerifyEmail";
+import HereSection from "./components/HereSection";
+import MainLayout from "./layout/MainLayout";
+
+
+
+
+
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+    
+        <MainLayout />
+     
+    ),
+    children: [
+      {
+        path: "/",
+        element: <HereSection />,
+      },
+      // {
+      //   path: "/profile",
+      //   element: <Profile />,
+      // },
+      // {
+      //   path: "/search/:text",
+      //   element: <SearchPage />,
+      // },
+      // {
+      //   path: "/restaurant/:id",
+      //   element: <RestaurantDetail />,
+      // },
+      // {
+      //   path: "/cart",
+      //   element: <Cart />,
+      // },
+      // {
+      //   path: "/order/status",
+      //   element: <Success />,
+      // },
+      // admin services start from here
+      // {
+      //   path: "/admin/restaurant",
+      //   element:<AdminRoute><Restaurant /></AdminRoute>,
+      // },
+      // {
+      //   path: "/admin/menu",
+      //   element:<AdminRoute><AddMenu /></AdminRoute>,
+      // },
+      // {
+      //   path: "/admin/orders",
+      //   element:<AdminRoute><Orders /></AdminRoute>,
+      // },
+    ],
+  },
+  {
+    path: "/login",
+    element:<Login />
+  },
+  {
+    path: "/signup",
+    element:<Signup /> ,
+  },
+  {
+    path: "/forgot-password",
+    element: <ForgotPassword />,
+  },
+  {
+    path: "/reset-password",
+    element: <ResetPassword />,
+  },
+  {
+    path: "/verify-email",
+    element: <VerifyEmail />,
+  },
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
-    <>
-      <div>
-        <Button>Hey Parveen</Button>
-       </div>
-    </>
-  )
+    <main>
+      <RouterProvider router={appRouter}></RouterProvider>
+    </main>
+  );
 }
 
-export default App
+export default App;
